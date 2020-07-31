ee3418824d69bfde0fc257fda467a2fcbc49603d
Jorge Barrera <jorge.barrera@globant.com>
2020-07-31 14:31:46 -0500

    Add project files.

---
+++ DecodeImages/DecodeImages.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace DecodeImages
+{
+    partial class DecodeImages
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button = new System.Windows.Forms.Button();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button.Location = new System.Drawing.Point(3, 3);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(114, 20);
+            this.button.TabIndex = 0;
+            this.button.Text = "SELECCIONAR";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // richTextBox
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.richTextBox, 3);
+            this.richTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox.Location = new System.Drawing.Point(3, 29);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBox.Size = new System.Drawing.Size(795, 423);
+            this.richTextBox.TabIndex = 1;
+            this.richTextBox.Text = "";
+            // 
+            // textBox
+            // 
+            this.textBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox.Location = new System.Drawing.Point(123, 3);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(554, 20);
+            this.textBox.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(683, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 20);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "DECODIFICAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.Controls.Add(this.button, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.richTextBox, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.934066F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 94.06593F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(801, 455);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // DecodeImages
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(801, 455);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "DecodeImages";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DecodeQR";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
+
---
+++ DecodeImages/DecodeImages.cs
@@ -0,0 +1,111 @@
+﻿using GhostscriptSharp;
+using GhostscriptSharp.Settings;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using ZXing;
+
+namespace DecodeImages
+{
+    public partial class DecodeImages : Form
+    {
+        public const string REGEX_MP_URL = @"https\:\/\/\S*\/pos\/(\d{@L})";
+        public const string REGEX_MP_LENGTH = @"com\.mercadolibre01(\d*)(http\S?\:\/\/\S*\/)";
+        public const int MP_OFFSET = 40;
+
+        public DecodeImages()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = folderBrowserDialog.ShowDialog();
+
+            if (dialogResult == DialogResult.OK)
+            {
+                textBox.Text = folderBrowserDialog.SelectedPath;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (FileInfo directoryInfoFile in GetFiles())
+            {
+                if (directoryInfoFile.Extension.Contains("pdf"))
+                {
+                    string pdf = directoryInfoFile.FullName;
+                    GhostscriptSettings settings = new GhostscriptSettings
+                    {
+                        Resolution = new Size(100, 100),
+                        Device = GhostscriptSharp.Settings.GhostscriptDevices.bmpgray,
+                        Size = new GhostscriptPageSize() { Manual = new Size(100, 100) },
+                    };
+                    GhostscriptSharp.GhostscriptWrapper.GenerateOutput(pdf, directoryInfoFile.FullName.Replace("pdf", "jpg"), settings);
+                    directoryInfoFile.Delete();
+                }
+            }
+
+            foreach (FileInfo directoryInfoFile in GetFiles())
+            {
+                // create a barcode reader instance
+                IBarcodeReader reader = new BarcodeReader();
+                // load a bitmap
+                var barcodeBitmap = (Bitmap)Image.FromFile(directoryInfoFile.FullName);
+                // detect and decode the barcode inside the bitmap
+                var result = reader.Decode(barcodeBitmap);
+                // do something with the result
+                if (result != null)
+                {
+                    richTextBox.AppendText(directoryInfoFile.Directory.Name + "\t" + directoryInfoFile.Name + "\t" + GetQrInfo(result.Text) + "\t" + (result.Text) + "\r\n");
+                }
+            }
+        }
+
+        private IEnumerable<FileInfo> GetFiles()
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(textBox.Text);
+            List<FileInfo> directoryInfoFiles = new List<FileInfo>();
+            directoryInfoFiles.AddRange(directoryInfo.EnumerateFiles().ToList());
+
+            foreach (var directoryInfoFolder in directoryInfo.EnumerateDirectories())
+            {
+                directoryInfoFiles.AddRange(directoryInfoFolder.EnumerateFiles().ToList());
+            }
+            return directoryInfoFiles;
+        }
+
+        private string GetQrInfo(string qrData)
+        {
+            try
+            {
+                Match match = Regex.Match(qrData, REGEX_MP_LENGTH);
+
+                if (match.Success)
+                {
+                    int urlLength = Convert.ToInt32(match.Groups[1].Value);
+                    int subUrlLength = urlLength - match.Groups[2].Value.Length;
+                    match = Regex.Match(qrData, REGEX_MP_URL.Replace("@L", subUrlLength.ToString()));
+
+                    if (match.Success)
+                    {
+                        return match.Groups[1].Value;
+                    }
+
+                    return qrData.Substring(MP_OFFSET, urlLength);
+                }
+
+                return string.Empty;
+
+            }
+            catch (Exception)
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
